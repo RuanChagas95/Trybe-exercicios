7f8d417eb8b4760926a6c568b24874ca31fb68d1
Ruan Chagas <ruanchagas95@gmail.com>
2023-05-05 15:58:49 -0300

    3.4 exercise 1.1 finish

--- fundamentos/JavaScript/3.4/objects.js
+++ fundamentos/JavaScript/3.4/objects.js
@@ -3,3 +3,5 @@ let info = {
     origem: 'Pato Donald',
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
   };
+  consoleText = `Bem-vinda, ${info.personagem}.`;
+  console.log(consoleText);
