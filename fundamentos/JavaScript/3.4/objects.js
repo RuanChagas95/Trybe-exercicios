09e4fc03afd645fc1f0fc1614a0b6d181e7e8f22
Ruan Chagas <ruanchagas95@gmail.com>
2023-05-05 16:06:18 -0300

    3.4 exercise 1.2 finish

--- fundamentos/JavaScript/3.4/objects.js
+++ fundamentos/JavaScript/3.4/objects.js
@@ -3,5 +3,10 @@ let info = {
     origem: 'Pato Donald',
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
   };
-  consoleText = `Bem-vinda, ${info.personagem}.`;
-  console.log(consoleText);
+// 1 - Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo o nome dela.
+consoleText = `Bem-vinda, ${info.personagem}.`;
+console.log(consoleText);
+//2 - Insira no objeto uma nova propriedade com o nome de chave ‘recorrente’ e o valor ‘Sim’ e, em seguida, imprima o objeto no console.
+// Object.assign(info, {recorrente: 'Sim'}) //boolean?
+info['recorrente'] = 'sim' //boolean
+console.log(info);
